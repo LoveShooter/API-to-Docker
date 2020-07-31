25606a73e8fa08cba527cfb260bd2f0f61011ea4
LoveShooter <maxxx.aka.subfocus@gmail.com>
2020-07-31 23:28:09 +0300

    upd

--- var/www/TestApp/start.sh
+++ var/www/TestApp/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 app="docker.test2"
 docker build -t ${app} .
-docker run -d -p 56733:80 \
-  --name=${app} \
-  -v $PWD:/app ${app}
+#docker run -d -p 56733:80 \
+  #--name=${app} \
+  #-v $PWD:/app ${app}
